e7243ebeb7eb0e61cffe982a6d851361e2a5018c
willy050209 <willy050209@gmail.com>
2025-02-25 11:05:27 +0800

    Create Super Mario

---
+++ Super_Mario/Super_Mario.cpp
@@ -0,0 +1,12 @@
+﻿// Super_Mario.cpp : Defines the entry point for the application.
+//
+
+#include "Super_Mario.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
