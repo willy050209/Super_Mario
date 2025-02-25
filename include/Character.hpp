7ea5dd67305349b2680b10ce7c008d9b70984dce
willy050209 <willy050209@gmail.com>
2025-02-25 20:06:32 +0800

    Create Character.hpp

---
+++ include/Character.hpp
@@ -0,0 +1,44 @@
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+#include "Util/GameObject.hpp"
+
+class Character : public Util::GameObject {
+public:
+    explicit Character(const std::string& ImagePath);
+
+    Character(const Character&) = delete;
+
+    Character(Character&&) = delete;
+
+    Character& operator=(const Character&) = delete;
+
+    Character& operator=(Character&&) = delete;
+
+    inline const std::string& GetImagePath() const noexcept { return m_ImagePath; }
+
+    inline const glm::vec2& GetPosition() const noexcept { return m_Transform.translation; }
+
+    inline bool GetVisibility() const noexcept { return m_Visible; }
+
+    void SetImage(const std::string& ImagePath) noexcept;
+
+    inline void SetPosition(const glm::vec2& Position) { m_Transform.translation = Position; }
+
+
+    inline bool IfCollides(const std::shared_ptr<Character>& other) const noexcept {
+        return fabs(other->GetPosition().x - GetPosition().x) <= 10.0 && fabs(other->GetPosition().y - GetPosition().y) <= 10.0;
+
+    }
+
+    virtual void move(const float& d) = 0;
+
+
+
+private:
+    inline void ResetPosition() noexcept { m_Transform.translation = { 0, 0 }; }
+
+    std::string m_ImagePath;
+};
+
+#endif // !CHARACTER_HPP
