1cea1f6ef1b6d454ce746f1668b73ab4ef16ba36
willy050209 <willy050209@gmail.com>
2025-02-25 20:06:29 +0800

    Update GameManager.cpp

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -4,20 +4,30 @@
 
 #include <memory>
 
-constexpr auto imagepath = "C:\\Users\\Willy\\Pictures\\Saved Pictures\\GepqrWqbsAI1gpA.jpg";
+constexpr auto BGMPath = MY_RESOURCE_DIR"\\BGM\\bgmtest.wav";
+constexpr auto BackgroundImagePath = MY_RESOURCE_DIR"\\Image\\Background\\cat.jpg";
+constexpr auto marioImagePath = MY_RESOURCE_DIR"\\Image\\Character\\bee_1.png";
 
 void GameManager::init() noexcept
 {
-	background = std::make_shared<Util::GameObject>(Util::GameObject(std::make_unique<Util::Image>(imagepath), -10));
+
+	bgm = std::make_shared<Util::BGM>(BGMPath);
+	bgm->SetVolume(64);// 0~128
+	background = std::make_shared<Util::GameObject>(Util::GameObject(std::make_unique<Util::Image>(BackgroundImagePath), 0));
+	mario = std::make_shared<Mario>(marioImagePath);
+
 	m_Root.AddChild(background);
+	m_Root.AddChild(mario);
+	bgm->Play();
 }
 
 void GameManager::Update() noexcept
 {
-	m_Root.Update();
 	if (Util::Input::IsKeyPressed(Util::Keycode::ESCAPE)) {
 		End();
 	}
+	mario->move(1.0);
+	m_Root.Update();
 }
 
 void GameManager::End() noexcept
