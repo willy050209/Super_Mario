23ca01f38ba4eba195deab072634a8391bb2f0d9
willy050209 <willy050209@gmail.com>
2025-02-27 20:57:49 +0800

    config

---
+++ PTSD/include/config.hpp
@@ -0,0 +1,25 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "pch.hpp" // IWYU pragma: export
+
+#include "Util/Logger.hpp"
+
+constexpr const char *TITLE = "Practical Tools for Simple Design";
+
+static int WINDOW_POS_X = SDL_WINDOWPOS_UNDEFINED;
+static int WINDOW_POS_Y = SDL_WINDOWPOS_UNDEFINED;
+
+static unsigned int WINDOW_WIDTH = 1600;
+static unsigned int WINDOW_HEIGHT = 900;
+
+constexpr Util::Logger::Level DEFAULT_LOG_LEVEL = Util::Logger::Level::DEBUG;
+
+/**
+ * @brief FPS limit
+ *
+ * Set value to 0 to turn off FPS cap
+ */
+static unsigned int FPS_CAP = 60;
+
+#endif
