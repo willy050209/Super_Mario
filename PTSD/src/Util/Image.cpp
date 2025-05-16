a26835b00d8a674242cfb1f6aa302b2d07a2446d
willy050209 <willy050209@gmail.com>
2025-05-16 15:33:40 +0800

    update

--- PTSD/src/Util/Image.cpp
+++ PTSD/src/Util/Image.cpp
@@ -71,7 +71,7 @@ void Image::InitProgram() {
         std::make_unique<Core::Program>(PTSD_ASSETS_DIR "/shaders/Base.vert",
                                         PTSD_ASSETS_DIR "/shaders/Base.frag");
     s_Program->Bind();
-
+    
     GLint location = glGetUniformLocation(s_Program->GetId(), "surface");
     glUniform1i(location, UNIFORM_SURFACE_LOCATION);
 }
--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -6,77 +6,34 @@
 #include "InitFormFunc.hpp"
 #include "Form/FormNames.hpp"
 
+#include <execution>
+#include <algorithm>
+#include <memory>
+#include <thread>
+
 
 
 void MyAPP::GameManager::init() noexcept {
-	//auto gobj = std::make_shared<Util::GameObject>(std::make_shared<Util::Image>(StairsBrickDarkImagePath), 10);
-	//std::unique_ptr<Util::Image> _;
-	/*std::thread t1([&gobj]() { auto _ = std::make_shared<Util::Image>(StairsBrickDarkImagePath); });
-	t1.join();*/
-	//auto _ = std::make_shared<Util::GameObject>(std::make_shared<Util::Image>(StairsBrickDarkImagePath), 0);
 
-	/*test BGM*/
-	/*bgms.push_back(std::make_shared<MyBGM::BGM>("Bgm", "D:\\program\\C++\\Super_Mario\\Resources\\BGM\\12. Ground Theme (Hurry!).wav"));
-	bgms.push_back(std::make_shared<MyBGM::BGM>("Bgm1", "D:/program/C++/Super_Mario/Resources/BGM/wakeup music.wav"));*/
-	int total = 7, current = 0;
+	//auto& _ = MyAPP::Form::Object::PositionReference::GetPositionReference();
+	//auto __ = std::make_unique<Util::Text>(MyAPP::MyResourcesFilePath::MyFontPath, 50, "test", Util::Color::FromName(Util::Colors::WHITE));
+	//
+	//auto idontknow = [](MyAPP::GameManager*) { 
+	//	auto __ = std::make_shared<Util::Image>(MyAPP::MyResourcesFilePath::EmptyBlockDarkImagePath);
+	//	auto ___ = std::make_unique<Util::Text>(MyAPP::MyResourcesFilePath::MyFontPath, 50, "test", Util::Color::FromName(Util::Colors::WHITE)); };
+	//
+	//std::thread t0(idontknow, this);
+	//t0.join();
+
 	puts("init GameManager");
-	/*bgms.push_back(std::make_shared<MyBGM::BGM>("Bgm1", MY_RESOURCE_DIR"/BGM/Ring08.wav"));
-	for (auto& it : bgms) {
-		it->Play();
-	}*/
-	/*auto button = std::make_shared<Button>("test", MyFontPath, 50, "pause", Util::Color::FromName(Util::Colors::SLATE_BLUE), 10);
-	button->SetPosition({ -GetX0(button), GetY0(button) });
-	button->userdata = bgms.back();
-	button->SetCallBackFunc([](Button* self, void* data) {
-		auto bgm = std::static_pointer_cast<MyBGM::BGM>(self->userdata);
-		if (bgm->isPlay()) {
-			bgm->Pause();
-		}
-		else {
-			bgm->Resum();
-		}
+	std::vector<std::function<void(MyAPP::GameManager*)>> initfuncs = {
+		initFormTitle, initForm_1_1, initFormOptions, initFormSetting
+	};
+	std::for_each(std::execution::seq, initfuncs.begin(), initfuncs.end(), [&](auto& func) {
+		(func)(this);
 	});
-	MyFM.addObject(Form_1_1, button);*/
-
-	//initFormBackground(this);
-	//showProgressBar(total, current++);
-	initFormTitle(this);
-	showProgressBar(total, current++);
-
-	initForm_1_1(this);
-	showProgressBar(total, current++);
-
-	//initForm_1_1_Pip(this);
-	showProgressBar(total, current++);
-
-	//initForm_1_2(this);
-	showProgressBar(total, current++);
-
-	initFormOptions(this);
-	showProgressBar(total, current++);
-
-	initFormSetting(this);
-	showProgressBar(total, current++);
-
-	//winForm(this);
-	showProgressBar(total, current++);
-
-	//initForm_1_1_to_1_2(this);
-	//initForm_1_2_Pipe(this);
-
-	//initForm_1_4(this);
-
-	MyFM.changeForm(MyAPP::Form::FormNames::FormTitel /*FormBackground*/);
-	//MyFM.changeForm(MyAPP::Form::FormNames::Form_1_4);
-	showProgressBar(total, current);
-	puts("");
-	/*system("pause");
-	for (auto& it : bgms) {
-		it->SetLoop(false);
-	}*/
-
-
-
+	MyFM.changeForm(MyAPP::Form::FormNames::FormTitel);
+	
 }
 
 void MyAPP::GameManager::Update(std::shared_ptr<Core::Context>& context) noexcept {
@@ -95,7 +52,4 @@ void MyAPP::GameManager::Update(std::shared_ptr<Core::Context>& context) noexcep
 
 void MyAPP::GameManager::End() noexcept {
 	endstate = true;
-	/*for (auto& bgm : bgms) {
-		bgm->Stop();
-	}*/
 }
