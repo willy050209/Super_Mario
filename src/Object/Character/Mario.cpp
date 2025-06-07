0563c405ecd0506c3d2a96c4783f073626304a77
willy050209 <willy050209@gmail.com>
2025-06-07 22:08:54 +0800

    Update Mario.cpp

--- src/Object/Character/Mario.cpp
+++ src/Object/Character/Mario.cpp
@@ -223,7 +223,7 @@ namespace MyAPP::Form::Object {
 		static const float PI = std::acos(-1);
 		static constexpr auto frequency = 0.4f; // Frequency of the sine wave
 		m_Transform.translation.x += distance.x;
-		m_Transform.translation.y = GetSize().y * std::sin(2 * PI * frequency * angle) + distance.y;
+		m_Transform.translation.y = GetSize().y * std::cos(2 * PI * frequency * angle) + distance.y;
 		angle += 0.055f; // Adjust the angle increment as needed for the desired arc effect
 	}
 
