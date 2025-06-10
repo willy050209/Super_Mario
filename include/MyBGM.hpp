6a6917cb1a0b171f83ed804be14fa211f0721dbb
willy050209 <willy050209@gmail.com>
2025-06-10 14:47:23 +0800

    don't use MyBGM

--- include/MyBGM.hpp
+++ include/MyBGM.hpp
@@ -1,94 +1,94 @@
-#pragma once
-#ifndef MYBGM_HPP
-#define MYBGM_HPP
-
-#include <string>
-#include <atomic>
-#include <thread>
-
-#pragma comment(lib, "winmm.lib")
-
-namespace MyBGM {
-
-	/// <summary>
-	/// Get Volume value (0 ~ F)
-	/// </summary>
-	/// <returns></returns>
-	int GetVolume() noexcept;
-
-	/// <summary>
-	/// Set Volume value (0 ~ F)
-	/// </summary>
-	/// <param name="Volume"></param>
-	void SetVolume(int Volume) noexcept;
-
-	class BGM {
-	public:
-		
-		BGM(const std::string& name) : name(name) {}
-
-		BGM(const std::string& name, const std::string& filePath) : name(name), filePath(filePath) {}
-
-		BGM(const BGM& other) = delete;
-
-		BGM(BGM&& other) = delete;
-
-		BGM() = delete;
-
-		~BGM() {
-			if (doloop->joinable()) {
-				(doloop)->join();
-			}
-		}
-
-		inline const int& GetLoop() const noexcept { return loop.load(); }
-
-		inline const std::string& GetFilePath() const noexcept { return filePath; }
-
-		inline const bool& isPause() const noexcept { return pause.load(); }
-
-		inline const bool& isPlay() const noexcept { return start.load(); }
-
-		inline void SetLoop(bool isloop) noexcept { loop.store(isloop); }
-
-		inline void SetFilePath(const std::string& filePath) noexcept { this->filePath = filePath; }
-
-		inline void SetFilePath(std::string&& filePath) noexcept { this->filePath = filePath; }
-
-
-		void Play() noexcept;
-
-		inline void Pause() noexcept {
-			pause.store(true);
-			start.store(false);
-			// loop.store(false);
-		}
-
-		inline void Resum() noexcept {
-			// pause.store(false);
-			start.store(true);
-		}
-
-		void Stop() noexcept {
-			exit.store(true);
-			loop.store(false);
-		}
-
-		void RePlay() noexcept;
-
-
-	private:
-		void PlayLoop() noexcept;
-
-		void PlayOnce() noexcept;
-
-		std::string filePath{ "" }, name{ "" };
-		std::atomic_bool loop{ true }, pause{ false }, exit{ false }, start{ false };
-		int pausePosition{ 0 };
-		std::shared_ptr<std::thread> doloop{ nullptr };
-	};
-}
-
-
-
-#endif // !MYBGM_HPP
+//#pragma once
+//#ifndef MYBGM_HPP
+//#define MYBGM_HPP
+//
+//#include <string>
+//#include <atomic>
+//#include <thread>
+//
+//#pragma comment(lib, "winmm.lib")
+//
+//namespace MyBGM {
+//
+//	/// <summary>
+//	/// Get Volume value (0 ~ F)
+//	/// </summary>
+//	/// <returns></returns>
+//	int GetVolume() noexcept;
+//
+//	/// <summary>
+//	/// Set Volume value (0 ~ F)
+//	/// </summary>
+//	/// <param name="Volume"></param>
+//	void SetVolume(int Volume) noexcept;
+//
+//	class BGM {
+//	public:
+//		
+//		BGM(const std::string& name) : name(name) {}
+//
+//		BGM(const std::string& name, const std::string& filePath) : name(name), filePath(filePath) {}
+//
+//		BGM(const BGM& other) = delete;
+//
+//		BGM(BGM&& other) = delete;
+//
+//		BGM() = delete;
+//
+//		~BGM() {
+//			if (doloop->joinable()) {
+//				(doloop)->join();
+//			}
+//		}
+//
+//		inline const int& GetLoop() const noexcept { return loop.load(); }
+//
+//		inline const std::string& GetFilePath() const noexcept { return filePath; }
+//
+//		inline const bool& isPause() const noexcept { return pause.load(); }
+//
+//		inline const bool& isPlay() const noexcept { return start.load(); }
+//
+//		inline void SetLoop(bool isloop) noexcept { loop.store(isloop); }
+//
+//		inline void SetFilePath(const std::string& filePath) noexcept { this->filePath = filePath; }
+//
+//		inline void SetFilePath(std::string&& filePath) noexcept { this->filePath = filePath; }
+//
+//
+//		void Play() noexcept;
+//
+//		inline void Pause() noexcept {
+//			pause.store(true);
+//			start.store(false);
+//			// loop.store(false);
+//		}
+//
+//		inline void Resum() noexcept {
+//			// pause.store(false);
+//			start.store(true);
+//		}
+//
+//		void Stop() noexcept {
+//			exit.store(true);
+//			loop.store(false);
+//		}
+//
+//		void RePlay() noexcept;
+//
+//
+//	private:
+//		void PlayLoop() noexcept;
+//
+//		void PlayOnce() noexcept;
+//
+//		std::string filePath{ "" }, name{ "" };
+//		std::atomic_bool loop{ true }, pause{ false }, exit{ false }, start{ false };
+//		int pausePosition{ 0 };
+//		std::shared_ptr<std::thread> doloop{ nullptr };
+//	};
+//}
+//
+//
+//
+//#endif // !MYBGM_HPP
--- src/MyBGM.cpp
+++ src/MyBGM.cpp
@@ -1,121 +1,121 @@
-#include "MyBGM.hpp"
-#include <windows.h>
-#include <mmsystem.h>
-#include <stdio.h>
-#include <memory>
-#include <iostream>
-
-using namespace MyBGM;
-
-void BGM::Play() noexcept {
-	if (isPause()) {
-		Resum();
-	}
-	else {
-		if (loop.load()) {
-			PlayLoop();
-		}
-		else {
-			PlayOnce();
-		}
-	}
-}
-
-void BGM::PlayLoop() noexcept {
-	if (doloop == nullptr) {
-		start.store(true);
-		doloop = std::make_shared<std::thread>([&]() {
-			this->exit.store(false);
-			do {
-				if (start.load()) {
-					// puts("play audio");
-					char command[256], retstr[64];
-					sprintf(command, "open \"%s\" alias \"%s\"", filePath.c_str(), name.c_str());
-					mciSendStringA(command, NULL, 0, NULL);
-					if (pause.load()) {
-						// puts("load audio");
-						//  std::cout << sprintf(command, "play \"%s\" from %d", name.c_str(), pausePosition) << pausePosition;
-						sprintf(command, "play \"%s\" from %d", name.c_str(), pausePosition);
-						mciSendStringA(command, NULL, 0, NULL);
-						pause.store(false);
-					}
-					else {
-						sprintf(command, "play \"%s\"", name.c_str());
-						mciSendStringA(command, NULL, 0, NULL);
-					}
-					sprintf(command, "status \"%s\" length", name.c_str());
-					mciSendStringA(command, retstr, sizeof(retstr), NULL);
-					int len = atoi(retstr);
-					do {
-						sprintf(command, "status \"%s\" position", name.c_str());
-						mciSendStringA(command, retstr, sizeof(retstr), NULL);
-						pausePosition = (atoi(retstr));
-
-					} while (!pause.load() && loop.load() && len > pausePosition);
-					sprintf(command, "stop \"%s\"", name.c_str());
-					mciSendStringA(command, retstr, sizeof(retstr), NULL);
-					sprintf(command, "close \"%s\"", name.c_str());
-					mciSendStringA(command, retstr, sizeof(retstr), NULL);
-					start.store(loop.load() && !pause.load());
-				}
-			} while (!this->exit.load());
-			// puts("exit loop");
-		});
-	}
-	else {
-		start.store(true);
-	}
-}
-
-void BGM::PlayOnce() noexcept {
-	if (doloop != nullptr && doloop->joinable()) {
-		Stop();
-		doloop->join();
-	}
-	doloop = std::make_shared<std::thread>([&]() {
-		this->exit.store(false);
-		char command[256], retstr[64];
-		sprintf(command, "open \"%s\" alias \"%s\"", filePath.c_str(), name.c_str());
-		mciSendStringA(command, NULL, 0, NULL);
-
-		sprintf(command, "play \"%s\"", name.c_str());
-		mciSendStringA(command, NULL, 0, NULL);
-
-		sprintf(command, "status \"%s\" length", name.c_str());
-		mciSendStringA(command, retstr, sizeof(retstr), NULL);
-
-		int len = atoi(retstr);
-
-		do {
-			sprintf(command, "status \"%s\" position", name.c_str());
-			mciSendStringA(command, retstr, sizeof(retstr), NULL);
-
-			pausePosition = (atoi(retstr));
-		} while (!this->exit && len > pausePosition);
-		sprintf(command, "stop \"%s\"", name.c_str());
-		mciSendStringA(command, retstr, sizeof(retstr), NULL);
-
-		sprintf(command, "close \"%s\"", name.c_str());
-		mciSendStringA(command, retstr, sizeof(retstr), NULL);
-	});
-}
-
-void BGM::RePlay() noexcept {
-	Stop();
-	Play();
-}
-
-int MyBGM::GetVolume() noexcept {
-	auto intptr = std::make_shared<DWORD>();
-	waveOutGetVolume(NULL, intptr.get());
-	return (*intptr & 0xf);
-}
-
-void MyBGM::SetVolume(int Volume) noexcept {
-	unsigned long tmp = 0;
-	for (int i = 0; i < 8; i++) {
-		tmp <<= 4;
-		tmp |= Volume;
-	}
-	waveOutSetVolume(NULL, tmp);
-}
+//#include "MyBGM.hpp"
+//#include <windows.h>
+//#include <mmsystem.h>
+//#include <stdio.h>
+//#include <memory>
+//#include <iostream>
+//
+//using namespace MyBGM;
+//
+//void BGM::Play() noexcept {
+//	if (isPause()) {
+//		Resum();
+//	}
+//	else {
+//		if (loop.load()) {
+//			PlayLoop();
+//		}
+//		else {
+//			PlayOnce();
+//		}
+//	}
+//}
+//
+//void BGM::PlayLoop() noexcept {
+//	if (doloop == nullptr) {
+//		start.store(true);
+//		doloop = std::make_shared<std::thread>([&]() {
+//			this->exit.store(false);
+//			do {
+//				if (start.load()) {
+//					// puts("play audio");
+//					char command[256], retstr[64];
+//					sprintf(command, "open \"%s\" alias \"%s\"", filePath.c_str(), name.c_str());
+//					mciSendStringA(command, NULL, 0, NULL);
+//					if (pause.load()) {
+//						// puts("load audio");
+//						//  std::cout << sprintf(command, "play \"%s\" from %d", name.c_str(), pausePosition) << pausePosition;
+//						sprintf(command, "play \"%s\" from %d", name.c_str(), pausePosition);
+//						mciSendStringA(command, NULL, 0, NULL);
+//						pause.store(false);
+//					}
+//					else {
+//						sprintf(command, "play \"%s\"", name.c_str());
+//						mciSendStringA(command, NULL, 0, NULL);
+//					}
+//					sprintf(command, "status \"%s\" length", name.c_str());
+//					mciSendStringA(command, retstr, sizeof(retstr), NULL);
+//					int len = atoi(retstr);
+//					do {
+//						sprintf(command, "status \"%s\" position", name.c_str());
+//						mciSendStringA(command, retstr, sizeof(retstr), NULL);
+//						pausePosition = (atoi(retstr));
+//
+//					} while (!pause.load() && loop.load() && len > pausePosition);
+//					sprintf(command, "stop \"%s\"", name.c_str());
+//					mciSendStringA(command, retstr, sizeof(retstr), NULL);
+//					sprintf(command, "close \"%s\"", name.c_str());
+//					mciSendStringA(command, retstr, sizeof(retstr), NULL);
+//					start.store(loop.load() && !pause.load());
+//				}
+//			} while (!this->exit.load());
+//			// puts("exit loop");
+//		});
+//	}
+//	else {
+//		start.store(true);
+//	}
+//}
+//
+//void BGM::PlayOnce() noexcept {
+//	if (doloop != nullptr && doloop->joinable()) {
+//		Stop();
+//		doloop->join();
+//	}
+//	doloop = std::make_shared<std::thread>([&]() {
+//		this->exit.store(false);
+//		char command[256], retstr[64];
+//		sprintf(command, "open \"%s\" alias \"%s\"", filePath.c_str(), name.c_str());
+//		mciSendStringA(command, NULL, 0, NULL);
+//
+//		sprintf(command, "play \"%s\"", name.c_str());
+//		mciSendStringA(command, NULL, 0, NULL);
+//
+//		sprintf(command, "status \"%s\" length", name.c_str());
+//		mciSendStringA(command, retstr, sizeof(retstr), NULL);
+//
+//		int len = atoi(retstr);
+//
+//		do {
+//			sprintf(command, "status \"%s\" position", name.c_str());
+//			mciSendStringA(command, retstr, sizeof(retstr), NULL);
+//
+//			pausePosition = (atoi(retstr));
+//		} while (!this->exit && len > pausePosition);
+//		sprintf(command, "stop \"%s\"", name.c_str());
+//		mciSendStringA(command, retstr, sizeof(retstr), NULL);
+//
+//		sprintf(command, "close \"%s\"", name.c_str());
+//		mciSendStringA(command, retstr, sizeof(retstr), NULL);
+//	});
+//}
+//
+//void BGM::RePlay() noexcept {
+//	Stop();
+//	Play();
+//}
+//
+//int MyBGM::GetVolume() noexcept {
+//	auto intptr = std::make_shared<DWORD>();
+//	waveOutGetVolume(NULL, intptr.get());
+//	return (*intptr & 0xf);
+//}
+//
+//void MyBGM::SetVolume(int Volume) noexcept {
+//	unsigned long tmp = 0;
+//	for (int i = 0; i < 8; i++) {
+//		tmp <<= 4;
+//		tmp |= Volume;
+//	}
+//	waveOutSetVolume(NULL, tmp);
+//}
