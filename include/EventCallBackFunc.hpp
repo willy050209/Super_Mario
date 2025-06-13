3b0a1ce71a53f2f18e369e657343363754602b7c
willy050209 <willy050209@gmail.com>
2025-06-13 10:58:29 +0800

    pdate

--- include/EventCallBackFunc.hpp
+++ include/EventCallBackFunc.hpp
@@ -544,8 +544,12 @@ EVENTCALLCALLBACKFUN(CheckFlagpoleCollision) {
 					timeEvent->Enable = false;
 					for (auto i = nowtime; i > 0; --i) {
 						GM->addPoint(50);
+						auto text = FM.GetFormObject<TextObject>(FM.GetNowForm(), "PointText");
+						std::unique_ptr<char> textstr(new char[10]);
+						sprintf(textstr.get(), "%06d", GM->GetPoint());
+						std::static_pointer_cast<Util::Text>(text->GetDrawable())->SetText(textstr.get());
 						FM.refresh();
-						std::this_thread::sleep_for(std::chrono::milliseconds(100));
+						//std::this_thread::;
 					}
 				}
 			}
